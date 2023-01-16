0ffb65aed29e1c5879792e0238cf798aa3ad10b1
adabas <36269224+adabas@users.noreply.github.com>
2023-01-16 14:55:40 +0100

    Updated parameter recovery script for stimuli version of models

--- Step3_parameterRecovery.m
+++ Step3_parameterRecovery.m
@@ -1,5 +1,4 @@
-% The script tests parameter recovery for the models: simple RW, random
-% responding, WSLS and RW-CK. This plots:
+% The script tests parameter recovery for the models: simple RW, WSLS, RW-CK and CK. This plots:
 % 1. Scatter plot with fitted vs simulated parameters
 % 2. Correlation between parameters of a) RW model and b) RW-CK model
 % 3. bias-variance trade off: is there is an overall bias in the estimated
@@ -9,7 +8,8 @@
 % This code is a modification of the code provided by Bob Wilson and Anne
 % Collins [2019].
 %
-% Modified by Aroma Dabas [dabas@cbs.mpg.de]
+% Updates by Aroma Dabas [dabas@cbs.mpg.de]
+%   01-2023     Cleaned and removed random responding model
 %   01-2020     Version 1
 % =========================================================================
 
@@ -22,17 +22,15 @@ rng(234, 'twister');
 
 % ================== Modify ===============================================
 % experiment parameters
-T       = 100;      % number of trials
+T       = 96;      % number of trials
 rbounds = [0 1];    % bounds of the mean reward
-nRep    = 100;     % number of simulation repetitions
-rprob   = [0.8 0.35]; % reward probability for [HR LR] stimuli
-Npt     = 0;       % number of partial trials
-%pbounds = [0 0 0 0 0 0;
-%    1 1 1 7 1 7];    % bounds [lower; upper] * parameters [b epsilon alpha beta alpha_c beta_c]
-
-pbounds = [0 0 0 0.05 0 0.05;
-    1 1 1 13 1 13];    % bounds [lower; upper] * parameters [b epsilon alpha beta alpha_c beta_c]
-
+nRep    = 50;       % number of simulation repetitions
+rprob   = [0.8 0.8 0.3 0.3]; % reward probability for [HR LR] stimuli
+Npt     = 0;        % number of partial trials
+pbounds = [0.5 0 0 0 0 0;     % parameter bounds updated to empirical data     
+  0.5 1 1 400 1 250];          % fixing null models parameter space to 0.5
+% pbounds = [0.5 0 0.05 0 0.05 0;     % parameter bounds updated to empirical data     
+%   0.5 1 1 400 1 250];          % fixing null models parameter space to 0.5
 
 % reward conditions
 cond        = {'High Reward', 'Low Reward'};
@@ -51,20 +49,15 @@ addpath('./LikelihoodFunctions')
 % some color settings for plotting
 AZred = [171,5,32]/256;
 AZblue = [12,35,75]/256;
-AZcactus = [92, 135, 39]/256;
-AZsky = [132, 210, 226]/256;
-AZriver = [7, 104, 115]/256;
-AZsand = [241, 158, 31]/256;
-AZmesa = [183, 85, 39]/256;
-AZbrick = [74, 48, 39]/256;
 
 savePlots   = 1;    % set as 1 if you want to save plots, otherwise 0
 plotFolder  = './Figures/ModelSimulation/';
 
 % model names, and corresponding parameters and subplot label
-names = {'RW model' 'RW model', 'RR model', 'WSLS model',...
-    'RW-CK', 'RW-CK', 'RW-CK', 'RW-CK'};
-symbols = {'\alpha' '\beta' 'b' '\epsilon' '\alpha' '\beta' '\alpha_c' '\beta_c'};
+names = {'RW model' 'RW model', 'WSLS model','RW-CK', 'RW-CK', 'RW-CK',...
+    'RW-CK', 'CK', 'CK'};
+symbols = {'\alpha' '\beta' '\epsilon' '\alpha' '\beta' '\alpha_c' '\beta_c',...
+    '\alpha_c', '\beta_c'};
 subplotLabel = {'A' 'B' 'C' 'D' 'E'};
 
 %% Section 2a: simulate and fit data for RW
@@ -77,10 +70,10 @@ for count = 1:nRep
     beta = exprnd(4);
     
     % simulate data
-    [choice, reward, pt, q] = simulate_M3RescorlaWagner_v1(T, alpha, beta, rprob, rbounds, Npt);
+    [choice, reward, pt, s] = simulate_M3RescorlaWagner_v2(T, alpha, beta, rprob, rbounds, Npt);
     
     % fit the data
-    [xf, LL] = fit_M3RescorlaWagner_v1(choice, reward, pt, pbounds(:, 3:4));
+    [xf, LL] = fit_M3RescorlaWagner_v2(choice, reward, pt, pbounds(:, 3:4), s);
 
     % store true values, estimated values, and - loglikelihood
     fminX.sim(1,count) = alpha;
@@ -94,27 +87,7 @@ for count = 1:nRep
 
 end
 
-%% Section 2b: simulate and fit data for random responding model
-
-for count = 1:nRep
-    % random parameter value for generating data
-    b = rand;
-    
-    % simulate data
-    [choice] = simulate_M1random_v1(T, rbounds, b, rprob, Npt);
-    
-    % fit the data
-    [xf, LL] = fit_M1random_v1(choice, pbounds(:,1));
-    
-    % store true values, estimated values and the - loglikelihood
-    fminX.sim(3,count) = b;
-    fminX.fit(3,count) = xf;
-    fminX.negLL(2,count) = LL;
-    
-    clear xf LL choice
-end
-
-%% Section 2c: simulate and fit data for noisy WSLS model
+%% Section 2b: simulate and fit data for noisy WSLS model
 
 for count = 1:nRep
     
@@ -122,20 +95,20 @@ for count = 1:nRep
     epsilon = rand;
     
     % simulate data
-    [choice, reward] = simulate_M2WSLS_v1(T, rbounds, epsilon, rprob, Npt);
+    [choice, reward, ~, s] = simulate_M2WSLS_v2(T, rbounds, epsilon, rprob, Npt);
     
     % fit the data
-    [xf, LL] = fit_M2WSLS_v1(choice, reward, pbounds(:,2));
+    [xf, LL] = fit_M2WSLS_v2(choice, reward, pbounds(:,2), s);
     
     % store true values, estimated values and the - loglikelihood
-    fminX.sim(4,count) = epsilon;
-    fminX.fit(4,count) = xf;
+    fminX.sim(3,count) = epsilon;
+    fminX.fit(3,count) = xf;
     fminX.negLL(3,count) = LL;
     
     clear xf LL choice reward
 end
 
-%% Section 2d: simulate and fit data for RW-CK model
+%% Section 2c: simulate and fit data for RW-CK model
 
 for count = 1:nRep
     
@@ -146,27 +119,52 @@ for count = 1:nRep
     beta_c  = exprnd(4);
     
     % simulate data
-    [choice, reward, pt, q] = simulate_M4RWCK_v1(T, alpha, beta, alpha_c,...
-        beta_c, rprob, rbounds, Npt);
+    [choice, reward, ~, s] = simulate_M4RWCK_v2(T, alpha, beta, alpha_c, beta_c,...
+        rprob, rbounds, Npt);
     
     % fit the data
-    [xf, LL] = fit_M4RWCK_v1(choice, reward, pt, pbounds(:, 3:6));
+    [xf, LL] = fit_M4RWCK_v2(choice, reward, [], pbounds(:, 3:6), s);
 
     % store true values, estimated values, and - loglikelihood
-    fminX.sim(5,count) = alpha;
-    fminX.sim(6,count) = beta;
-    fminX.sim(7,count) = alpha_c;
-    fminX.sim(8,count) = beta_c;
-    fminX.fit(5,count) = xf(1);
-    fminX.fit(6,count) = xf(2);
-    fminX.fit(7,count) = xf(3);
-    fminX.fit(8,count) = xf(4);
+    fminX.sim(4,count) = alpha;
+    fminX.sim(5,count) = beta;
+    fminX.sim(6,count) = alpha_c;
+    fminX.sim(7,count) = beta_c;
+    fminX.fit(4,count) = xf(1);
+    fminX.fit(5,count) = xf(2);
+    fminX.fit(6,count) = xf(3);
+    fminX.fit(7,count) = xf(4);
     fminX.negLL(4,count) = LL;
 
     % clear repeating variables from the workspace
     clear xf LL choice reward pt
 end
 
+%% Section 2d: simulate and fit data for CK model
+
+for count = 1:nRep
+    
+    % random values for generating data
+    alpha_c = rand;
+    beta_c  = exprnd(4);
+    
+    % simulate data
+    [choice, reward, ~, s] = simulate_M5CK_v2(T, alpha_c, beta_c,...
+        rprob, rbounds, Npt);
+    
+    % fit the data
+    [xf, LL] = fit_M5ChoiceKernel_v2(choice, reward, [], pbounds(:, 5:6), s);
+
+    % store true values, estimated values, and - loglikelihood
+    fminX.sim(8,count) = alpha_c;
+    fminX.sim(9,count) = beta_c;
+    fminX.fit(8,count) = xf(1);
+    fminX.fit(9,count) = xf(2);
+    fminX.negLL(5,count) = LL;
+
+    % clear repeating variables from the workspace
+    clear xf LL choice reward pt
+end
 
 %% Section 3: basic parameter recovery plots (true vs estimated)
 
@@ -190,10 +188,10 @@ end
 % mark the 'bad' parameter values
 for i = 1:size(fminX.sim,1)
     % determine threshold for 'bad' parameter values
-    if i == 1 || i == 3 || i == 4
+    if i == 1 || i == 3 || i == 4 || i == 6 || i == 8
         thresh = 0.25;
-    elseif i == 2 || i == 6 || i == 8
-        thresh = 5;
+    elseif i == 2 || i == 5 || i == 7 || i == 9
+        thresh = 25;
     end
     
     % parameter values that exceed the threshold
@@ -222,15 +220,12 @@ for i = 1:size(names, 2)
     % set font and tick sizes
     ax = gca;
     set(ax, 'tickdir', 'out', 'fontsize', 12);
-    
-    % add subplot label
-  %  addABCs(ax, [-0.06 0.05], 12, subplotLabel(i));
 end
 
 % save figure
 if savePlots
     fh.PaperPositionMode = 'auto';
-    filename = fullfile(plotFolder, 'Parameter_recovery', 'fit-vs-true.png');
+    filename = fullfile(plotFolder, 'ParameterRecovery', 'fit-vs-true.png');
     saveas(gcf, filename)
 end
 
@@ -244,37 +239,30 @@ parTable = table(alpha, beta);
 
 % plot the table and check if the correlation is significant
 corrplot(parTable, 'type', 'Spearman', 'testR','on', 'rows', 'pairwise');
-% th = findall(fh_cor(1).Parent.Parent, 'type', 'text', 'String', '{\bf Correlation Matrix}'); 
-% th.String = 'RW';
-%annotation('textbox', [.02 .01 .29 .05], 'String', 'estimated parmeters');
 
 % save figure
 if savePlots
 %     fh2.PaperPositionMode = 'auto';
-    filename = fullfile(plotFolder, 'Parameter_recovery', 'fit-vs-fit_RW.png');
+    filename = fullfile(plotFolder, 'ParameterRecovery', 'RW_fittedAlphaBetaCorrelation.png');
     saveas(gcf, filename)
 end
 % ---
 
 % RW CK
 % create table containing parameter values
-alpha = fminX.fit(5,:)';
-beta = fminX.fit(6,:)';
-alphaCK = fminX.fit(7,:)';
-betaCK = fminX.fit(8,:)';
+alpha = fminX.fit(4,:)';
+beta = fminX.fit(5,:)';
+alphaCK = fminX.fit(6,:)';
+betaCK = fminX.fit(7,:)';
 parTable = table(alpha, beta, alphaCK, betaCK);
 
 % plot the table and check if the correlation is significant
-
 corrplot(parTable, 'type', 'Spearman', 'testR','on', 'rows', 'pairwise');
-% th = findall(fh_cor(1).Parent.Parent, 'type', 'text', 'String', '{\bf Correlation Matrix}'); 
-% th.String = 'RW';
-%annotation('textbox', [.02 .01 .29 .05], 'String', 'estimated parmeters');
 
 % save figure
 if savePlots
 %     fh2.PaperPositionMode = 'auto';
-    filename = fullfile(plotFolder, 'Parameter_recovery', 'fit-vs-fit_RW.png');
+    filename = fullfile(plotFolder, 'ParameterRecovery', 'RWCK_fittedAlphaBetaCorrelation.png');
     saveas(gcf, filename)
 end
 
@@ -301,8 +289,7 @@ end
 % print on window
 fprintf('\nRMSE values\nalpha = %.3f\n', fminX.RMSE(1,:))
 fprintf('beta = %.3f\n', fminX.RMSE(2,:))
-fprintf('b = %.3f\n', fminX.RMSE(3,:))
-fprintf('epsilon = %.3f\n', fminX.RMSE(4,:))
+fprintf('epsilon = %.3f\n', fminX.RMSE(3,:))
 % --
 
 % PLOT
@@ -329,19 +316,17 @@ fprintf('epsilon = %.3f\n', fminX.RMSE(4,:))
      
      % add label
      xlabel(sprintf('%s', symbols{i}));
-     ylabel(sprintf('true - estimated error'));
      
      % set font and tick sizes
      ax = gca;
      set(ax, 'tickdir', 'out', 'fontsize', 11);
-     
-     % add subplot label
-%      addABCs(ax, [-0.06 0.05], 11, subplotLabel(i));
- end
 
+ end
+title('true - estimated error');
+ 
 % save plot
 if savePlots
-    filename = fullfile(plotFolder, 'Parameter_recovery', 'biasVariance_allModels.png');
+    filename = fullfile(plotFolder, 'ParameterRecovery', 'biasVariance_allModels.png');
     saveas(gcf, filename)
 end
 
