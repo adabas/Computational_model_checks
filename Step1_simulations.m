06642b0dbd36b45853c9acc97ad553600311b039
adabas <36269224+adabas@users.noreply.github.com>
2023-07-17 11:37:40 +0200

    updated to version 2 of model scripts

--- Step1_simulations.m
+++ Step1_simulations.m
@@ -45,10 +45,10 @@ addpath(genpath('./HelperFunctions'))
 rng(7, 'twister');
 
 % experiment parameters
-T       = 100;       % number of trials
+T       = 96;       % number of trials
 rbounds = [0 1];     % bounds of the mean reward
-nrep    = 40;        % number of simulation repetitions
-rprob   = [0.8 0.35]; % reward probability for [HR LR] stimuli 
+nrep    = 50;        % number of simulation repetitions
+rprob   = [0.8 0.8 0.3 0.3]; %[0.8 0.3]; % reward probability for [HR LR] stimuli 
 Npt     = 0;        % number of partial trials
 
 % specify the number and name of the model that you are interested in
@@ -57,14 +57,15 @@ nModels     = [1 2 3 4 5];
 nameModels  = {'Random responding', 'Noisy WSLS', 'RW', 'RW-CK', 'CK'};
 
 % for plotting
-savePlots   = 1;    % set as 1 if you want to save plots, otherwise 0
+savePlots   = 0;    % set as 1 if you want to save plots, otherwise 0
 plotFolder  = './Figures/ModelSimulation/';
 
 %% Section 2: simulate
 % Model 1: Random responding
 for n = 1:nrep
     b = 0.5;    % initiate bias parameter
-    [a, r] = simulate_M1random_v1(T, rbounds, b, rprob, Npt); % simulate data
+    %[a, r] = simulate_M1random_v1(T, rbounds, b, rprob, Npt); % simulate data
+    [a, r] = simulate_M1random_v2(T, rbounds, b, rprob);
     sim(1).a(:,n) = a;  
     sim(1).r(:,n) = r;
 end
@@ -73,7 +74,8 @@ clear a r
 % Model 2: Noisy Win-stay-lose-shift
 for n = 1:nrep
     epsilon = 0.3; % probability of selecting an option
-    [a, r] = simulate_M2WSLS_v1(T, rbounds, epsilon, rprob, Npt);
+    %[a, r] = simulate_M2WSLS_v1(T, rbounds, epsilon, rprob, Npt);
+    [a, r] = simulate_M2WSLS_v2(T, rbounds, epsilon, rprob); 
     sim(2).a(:,n) = a;
     sim(2).r(:,n) = r;
 end
@@ -83,7 +85,8 @@ clear a r
 for n = 1:nrep
     alpha   = 0.1; 
     beta    = 5; 
-    [a, r] = simulate_M3RescorlaWagner_v1(T, alpha, beta, rprob, rbounds, Npt);
+    %[a, r] = simulate_M3RescorlaWagner_v1(T, alpha, beta, rprob, rbounds, Npt);
+    [a, r] = simulate_M3RescorlaWagner_v2(T, alpha, beta, rprob, rbounds);
     sim(3).a(:,n) = a;
     sim(3).r(:,n) = r;
 end
@@ -91,9 +94,12 @@ clear a r
 
 % Model 4: Rescorla Wagner with Choice kernel
 for n = 1:nrep
+    alpha   = 0.1; 
+    beta    = 5;
     alpha_c = 0.1;
     beta_c = 3;
-    [a, r] = simulate_M4RWCK_v1(T, alpha, beta, alpha_c, beta_c, rprob, rbounds, Npt);
+    %[a, r] = simulate_M4RWCK_v1(T, alpha, beta, alpha_c, beta_c, rprob, rbounds, Npt);
+    [a, r] = simulate_M4RWCK_v2(T, alpha, beta, alpha_c, beta_c, rprob, rbounds);
     sim(4).a(:,n) = a;
     sim(4).r(:,n) = r;
 end
@@ -101,7 +107,10 @@ clear a r
 
 % Model 5: Choice kernel
 for n = 1:nrep
-    [a, r] = simulate_M5CK_v1(T, alpha_c, beta_c, rprob, rbounds, Npt);
+    alpha_c = 0.1;
+    beta_c = 3;
+    %[a, r] = simulate_M5CK_v1(T, alpha_c, beta_c, rprob, rbounds, Npt);
+    [a, r] = simulate_M5CK_v2(T, alpha_c, beta_c, rprob, rbounds);
     sim(5).a(:,n) = a;
     sim(5).r(:,n) = r;
 end
@@ -113,13 +122,19 @@ clear a r
 
 % ======================= Plot choice probability =========================
 
+% convert choices (coded 1 to 4) to reward conditions: HR: 1 & LR: 0
+for i_mod = 1:length(nModels)
+    sim(i_mod).a(sim(i_mod).a == 2) = 1;
+    sim(i_mod).a(sim(i_mod).a == 3 | sim(i_mod).a == 4) = 0;
+end
+
 % initiate the plot
 fh.score = figure('Name','score'); 
 set(fh.score,'position',[10 90 1400 400], 'paperunits','centimeters','Color','w');
 set(gca, 'fontsize', 12)
 
-% extract high-reward choice option
-highRewAction = find(rprob==max(rprob));
+% % extract high-reward choice option
+%highRewAction = find(rprob==max(rprob));
 
 % loop over the number of models 
 for m = 1:length(nModels)
@@ -130,14 +145,14 @@ for m = 1:length(nModels)
     subplot(1, length(nModels), m)
     
     % store choices for the model
-    choice = sim(m).a;
+    highRewChoice = sim(m).a; %choice = sim(m).a;
     
-    % convert the scoring such as the HR is 1 and LR is 0
-    if highRewAction == 2
-        highRewChoice = 2 - choice; 
-    else
-        highRewChoice = choice -1;
-    end
+%     % convert the scoring such as the HR is 1 and LR is 0
+%     if highRewAction == 2
+%         highRewChoice = 2 - choice; 
+%     else
+%         highRewChoice = choice -1;
+%     end
         
     % calculate the probability that HR was selected
     score = (nansum(highRewChoice == 1, 1))/T;
@@ -201,12 +216,12 @@ for m = 1:length(nModels)
     % calculate HR choice mean over each trial
     HR_choiceMean = nanmean(sim(m).a, 2);
     
-    % rescale the choice mean such that it ranges between 0 (LR) and 1 (HR)
-    if highRewAction == 2
-        HR_choiceMean = (2 - HR_choiceMean)';
-    else
-        HR_choiceMean = (HR_choiceMean - 1)';
-    end
+%     % rescale the choice mean such that it ranges between 0 (LR) and 1 (HR)
+%     if highRewAction == 2
+%         HR_choiceMean = (2 - HR_choiceMean)';
+%     else
+%         HR_choiceMean = (HR_choiceMean - 1)';
+%     end
     
     % add data to the plot
     line([0, length(HR_choiceMean)], [rprob(1), rprob(1)],...
@@ -214,7 +229,7 @@ for m = 1:length(nModels)
     line([0, length(HR_choiceMean)], [rprob(2), rprob(2)],...
         'LineStyle', '--', 'Color', AZcactus, 'linewidth',0.5);  hold on
     plot(HR_choiceMean, ':', 'color', AZred, 'linewidth',0.5)
-    plot(mySmooth(HR_choiceMean, smoothingkernel,[], 'backward'),...
+    plot(mySmooth(HR_choiceMean', smoothingkernel,[], 'backward'),...
         '-','color', AZred,'linewidth',1); 
 
     % y axis limits
@@ -257,9 +272,10 @@ if savePlots
     saveas(gcf, filename)
 end
 
-%% p(stay), also referred to as win stay lose shift (WSLS) analysis
+%% p(stay)
 % determine the times the agent reselects the option that previously gave a
-% pleasant, neutral & unpleasant reward.
+% pleasant, & unpleasant reward. Here we use the choice conditions instead
+% of the individual 4 choices.
 
 % loop over the simulations
 for i = 1:length(sim)
@@ -275,7 +291,7 @@ for i = 1:length(sim)
     wsls(:,i) = nanmean(sim(i).wsls,2);
 end
 
-%% Plot WSLS behavior for all models
+%% Plot WSLS (condition version) behavior for all models
 
 % open figure
 figure(3); clf; hold on;
@@ -288,7 +304,7 @@ ylim([0 1])
 
 % set plot and tick properties
 set(l, 'marker', '.', 'markersize', 50, 'linewidth', 3)
-set(gca, 'xtick', [1 2 3], 'XTickLabel', {'Unpleasant', 'Neutral', 'Pleasant'}, ...
+set(gca, 'xtick', [1 2], 'XTickLabel', {'Unpleasant', 'Pleasant'}, ...
     'tickdir', 'out', 'fontsize', 14, 'xlim', [0.5 3.5])
 
 % set title and labels
