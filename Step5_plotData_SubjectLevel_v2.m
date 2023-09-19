0b771835e86608a1b8e26e4653c828f22c295212
Aroma Dabas <dabas@cbs.mpg.de>
2023-09-19 15:07:18 +0000

    parameter bounds information

--- Step5_plotData_SubjectLevel_v2.m
+++ Step5_plotData_SubjectLevel_v2.m
@@ -18,7 +18,7 @@ savePlots   = true;     % true will save plots in plotFolder directory
 plotFolder  = "./Figures/SubjectLevel";
 trialSeq       = 1:96;     % for plotting
 
-% specify the bounds, and the number of bins for grid search
+% parameter bounds [lower; upper] * parameters [b epsilon alpha(RW) beta(RW) alpha(CK) beta(CK) alpha_c beta_c]
 pbounds = [0 0 0 0 0 0;     % parameter bounds updated to empirical data     
   1 1 1 400 1 250];
 
